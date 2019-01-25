e690c3e9b8d6ed32810b8567670ff2993674477e
Robin Bate Boerop <robin@engineml.com>
2019-01-24 16:19:13 -0800

    Correct tmux

--- modules/tmux/interactive.sh
+++ modules/tmux/interactive.sh
@@ -4,7 +4,7 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-tmux() {
+upshell_tmux() {
   if [ -d "$XDG_CONFIG_HOME"/tmux ] ; then
      tmux -f "$XDG_CONFIG_HOME"/tmux/tmux.conf "$@"
   elif [ -d "$HOME"/.config/tmux ] ; then
@@ -17,3 +17,5 @@ tmux() {
 if [ "$XDG_RUNTIME_DIR" ] ; then
    export TMUX_TMPDIR="$XDG_RUNTIME_DIR"
 fi
+
+alias tmux=upshell_tmux
