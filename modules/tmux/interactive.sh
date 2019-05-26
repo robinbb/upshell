775c44ca5d1aa7ab3fab13e6bd5a75362daac785
Robin Bate Boerop <robin@engineml.com>
2019-05-26 11:15:27 -0700

    Correct tmux function

--- modules/tmux/interactive.sh
+++ modules/tmux/interactive.sh
@@ -4,18 +4,16 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-upshell_tmux() {
+tmux() {
   if [ -d "$XDG_CONFIG_HOME"/tmux ] ; then
-     tmux -f "$XDG_CONFIG_HOME"/tmux/tmux.conf "$@"
+     command tmux -f "$XDG_CONFIG_HOME"/tmux/tmux.conf "$@"
   elif [ -d "$HOME"/.config/tmux ] ; then
-     tmux -f "$HOME"/.config/tmux/tmux.conf "$@"
+     command tmux -f "$HOME"/.config/tmux/tmux.conf "$@"
   else
-     tmux "$@"
+     command tmux "$@"
   fi
 }
 
 if [ "$XDG_RUNTIME_DIR" ] ; then
    export TMUX_TMPDIR="$XDG_RUNTIME_DIR"
 fi
-
-alias tmux=upshell_tmux
