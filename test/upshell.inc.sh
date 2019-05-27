81bb2dff593162de7f7e0a4399310cc113a6baa1
Robin Bate Boerop <robin@engineml.com>
2019-05-27 11:41:36 -0700

    Progress

--- test/upshell.inc.sh
+++ test/upshell.inc.sh
@@ -1,29 +1,151 @@
 #! /bin/sh
 
-set -eu
+set -e
 set -x
 
 dir="$(dirname "$0")"
+
+# Check formatting of the file under test.
 shfmt -p -i 3 -ci -sr -d "$dir"/../upshell.inc.sh
+
+# Check formatting of this file.
 shfmt -p -i 3 -ci -sr -d "$0"
 
+# Include the file under test.
 . "$dir"/../upshell.inc.sh
+
+# Unit Tests
+
+# upshell_ord
 
 [ "$(upshell_ord A)" = '65' ]
 [ "$(upshell_ord a)" = '97' ]
 [ "$(upshell_ord @)" = '64' ]
 
+# upshell_chr
+
 [ "$(upshell_chr 65)" = 'A' ]
 [ "$(upshell_chr 97)" = 'a' ]
 [ "$(upshell_chr 64)" = '@' ]
 
+# upshell_ascii2hex
+
 [ "$(upshell_ascii2hex A)" = '41' ]
 [ "$(upshell_ascii2hex 0)" = '30' ]
 [ "$(upshell_ascii2hex Z)" = '5a' ]
-[ "$(upshell_ascii2hex a)" = '30' ]
-[ "$(upshell_ascii2hex z)" = '30' ]
-[ "$(upshell_ascii2hex @)" = '30' ]
-[ "$(upshell_ascii2hex /)" = '30' ]
+[ "$(upshell_ascii2hex a)" = '61' ]
+[ "$(upshell_ascii2hex z)" = '7a' ]
+[ "$(upshell_ascii2hex @)" = '40' ]
+[ "$(upshell_ascii2hex /)" = '2f' ]
+[ "$(upshell_ascii2hex '')" = '0' ]
 
-# printf %s "416142624363" |
-#   upshell_
+# upshell_hex2ascii
+
+[ "$(upshell_hex2ascii 41)" = 'A' ]
+[ "$(upshell_hex2ascii 30)" = '0' ]
+[ "$(upshell_hex2ascii 0)" = '' ]
+[ "$(upshell_hex2ascii 40)" = '@' ]
+
+# upshell_split_string
+
+[ "$(printf %s "416142624363" | upshell_split_string)" \
+   = '4
+1
+6
+1
+4
+2
+6
+2
+4
+3
+6
+3' ]
+[ "$(printf %s "string with spaces!" | upshell_split_string)" \
+   = 's
+t
+r
+i
+n
+g
+ 
+w
+i
+t
+h
+ 
+s
+p
+a
+c
+e
+s
+!' ]
+
+# upshell_split_pairs
+
+[ "$(printf %s "416142624363" | upshell_split_pairs)" \
+   = '41
+61
+42
+62
+43
+63' ]
+
+[ "$(printf %s "416142624363" | upshell_hex2string)" \
+   = 'AaBbCc' ]
+[ "$(printf %s "416142624363" | upshell_string2hex)" \
+   = '343136313432363234333633' ]
+[ "$(printf %s 'git@github.com/robinbb' | upshell_string2hex)" \
+   = '676974406769746875622e636f6d2f726f62696e6262' ]
+[ "$(printf %s '676974406769746875622e636f6d2f726f62696e6262' |
+   upshell_hex2string)" = 'git@github.com/robinbb' ]
+
+# Property Tests
+
+# Reversibility of upshell_hex2string / upshell_string2hex
+
+test_strings="
+abc
+123
+a1b2c3
+https://github.com
+string with spaces!
+strange characters:
+_
+-
+=
+==
+\\
+\'
+git@github.com/robinbb/upshell.git
+"
+
+printf %s "$test_strings" | while IFS= read -r str; do
+   hex="$(printf %s "$str" | upshell_string2hex)"
+   [ "$str" = "$(printf %s "$hex" | upshell_hex2string)" ]
+done
+
+# upshell_require
+
+export UPSHELL_ERROR_FD=3
+upshell_require sh
+upshell_require awk
+upshell_require tr
+upshell_require git
+upshell_require shfmt
+! upshell_require exec-not-found-blah
+[ "$(upshell_require exec-not-found-blah 3>&1)" \
+   = "The 'exec-not-found-blah' executable is required, but is not on the PATH.
+Failure." ]
+
+# upshell_delete_cache, upshell_list, upshell_clone, upshell_url2hex
+upshell_delete_cache
+[ "$(upshell_list)" = '' ]
+upshell_clone https://github.com/robinbb/upshell
+hexdir="$(upshell_url2hex https://github.com/robinbb/upshell)"
+[ -e "$UPSHELL_CACHE_HOME"/"$hexdir" ]
+[ "$(upshell_list)" = 'https://github.com/robinbb/upshell' ]
+
+# Clearly indicate completion.
+echo 'Success!'
--- upshell.inc.sh
+++ upshell.inc.sh
@@ -4,6 +4,12 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+upshell_init() {
+   : "${UPSHELL_ERROR_FD:=2}"
+   : "${UPSHELL_CACHE_HOME:=${XDG_CACHE_HOME:-${HOME}/.cache}/upshell}"
+   : "${UPSHELL_CONFIG_HOME:=${XDG_CONFIG_HOME:-${HOME}/.config}/upshell}"
+}
+
 upshell_ord() {
    LC_CTYPE=C printf %d "'$1"
 }
@@ -21,17 +27,101 @@ upshell_hex2ascii() {
    printf "\\x$1"
 }
 
-#upshell_split_pairs() {
-#  awk '{ gsub("..", "&\n") ; printf("%s",$0) }'
-#}
-#
-#upshell_hex2string() {
-#  upshell_split_pairs |
-#     while read pair; do
-#        upshell_hex2ascii "$pair"
-#     done
-#}
-#
-#upshell() {
-#   :
-#}
+upshell_split_string() {
+   awk '{ gsub(".", "&\n") ; printf("%s", $0) }'
+}
+
+upshell_split_pairs() {
+   awk '{ gsub("..", "&\n") ; printf("%s", $0) }'
+}
+
+upshell_hex2string() {
+   upshell_split_pairs |
+      while IFS= read -r pair; do
+         upshell_hex2ascii "$pair"
+      done
+}
+
+upshell_string2hex() {
+   upshell_split_string |
+      while IFS= read -r char; do
+         upshell_ascii2hex "$char"
+      done
+}
+
+upshell_err() {
+   echo "$1" >&"$UPSHELL_ERROR_FD"
+}
+
+upshell_fail() {
+   upshell_err "$1"
+   upshell_err "Failure."
+   return 1
+}
+
+upshell_require() {
+   command -v "$1" > /dev/null ||
+      upshell_fail "The '$1' executable is required, but is not on the PATH."
+}
+
+upshell_url2hex() {
+   printf '%s' "$1" | upshell_string2hex
+}
+
+upshell_hex2url() {
+   printf '%s' "$1" | upshell_hex2string
+}
+
+upshell_list() {
+   if [ -d "$UPSHELL_CACHE_HOME" ]; then
+      (
+         CDPATH= cd -- "$UPSHELL_CACHE_HOME"
+         for dir in *; do
+            [ -d "$dir" ] || continue
+            echo "$(upshell_hex2url "$dir")"
+         done
+      )
+   fi
+}
+
+upshell_delete_cache() {
+   [ "$UPSHELL_CACHE_HOME" ] &&
+      [ -d "$UPSHELL_CACHE_HOME" ] &&
+      (
+         CDPATH= cd -- "$UPSHELL_CACHE_HOME"
+         for dir in *; do
+            [ -d "$dir" ] || continue
+            echo Removing "$(upshell_hex2url "$dir")"
+            rm -fr "$dir"
+         done
+      )
+}
+
+upshell_clone() (
+   repo_dir="$UPSHELL_CACHE_HOME"/"$(upshell_url2hex "$1")"
+   if [ ! -e "$repo_dir" ]; then
+      upshell_require git
+      git clone \
+         --depth 1 \
+         --recursive \
+         -b master \
+         "$1" \
+         "$repo_dir" ||
+         upshell_fail "git clone failed"
+   fi
+)
+
+upshell() {
+   case "$1" in
+      'list')
+         shift
+         upshell_list "$@"
+         ;;
+      *)
+         upshell_clone "$@"
+         ;;
+   esac
+}
+
+upshell_init
+# upshell "$@"
